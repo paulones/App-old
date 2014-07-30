d70e017accf51a8e147e4f3e15c079e164b1d29b
Paulo <paulones89@gmail.com>
2014-07-29 21:23:25 -0300

    commit form validation

--- web/assets/admin/pages/scripts/pf-cadastrar.js
+++ web/assets/admin/pages/scripts/pf-cadastrar.js
@@ -1,7 +1,66 @@
 var PFCad = function() {
+    
+    var handleValidation = function() {
+        // for more info visit the official plugin documentation: 
+            // http://docs.jquery.com/Plugins/Validation
 
+            var form = $('#form');
+            var error = $('.alert-danger', form);
+            var success = $('.alert-success', form);
+
+            form.validate({
+                errorElement: 'span', //default input error message container
+                errorClass: 'help-block help-block-error', // default input error message class
+                focusInvalid: false, // do not focus the last invalid input
+                ignore: "",  // validate all fields including form hidden input
+                rules: {
+                    name: {
+                        minlength: 2,
+                        required: true
+                    },
+                },
+
+                invalidHandler: function (event, validator) { //display error alert on form submit              
+                    success.hide();
+                    error.show();
+                    Metronic.scrollTo(error, -200);
+                },
+
+                errorPlacement: function (error, element) { // render error placement for each input type
+                    var icon = $(element).parent('.input-icon').children('i');
+                    icon.removeClass('fa-check').addClass("fa-warning");  
+                    icon.attr("data-original-title", error.text()).tooltip({'container': 'body'});
+                    icon.trigger("mouseenter");
+                },
+
+                highlight: function (element) { // hightlight error inputs
+                    $(element)
+                        .closest('.form-group').removeClass("has-success").addClass('has-error'); // set error class to the control group   
+                },
+
+                unhighlight: function (element) { // revert the change done by hightlight
+                    
+                },
+
+                success: function (label, element) {
+                    var icon = $(element).parent('.input-icon').children('i');
+                    $(element).closest('.form-group').removeClass('has-error').addClass('has-success'); // set success class to the control group
+                    icon.removeClass("fa-warning").addClass("fa-check");
+                    icon.trigger("mouseleave");
+                },
+
+                submitHandler: function (form) {
+                    success.show();
+                    error.hide();
+                }
+            });
+    }
+    
     return {
         init: function() {
+            
+            handleValidation();
+            
             $('.menu-pf').addClass('active open');
             $('.menu-pf a').append('<span class="selected"></span>');
             $('.menu-pf a .arrow').addClass('open');
@@ -9,4 +68,4 @@ var PFCad = function() {
         }
     };
 }();
-
+    
