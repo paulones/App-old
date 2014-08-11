a7265628e0c97ed6c53f33d8313e5e0f59c14ca4
Pedro <Pedro@SGN-DESEN_10>
2014-08-11 20:44:46 -0300

    Correção da seleção do radio box de atividade

--- web/assets/admin/pages/scripts/pj-cadastrar.js
+++ web/assets/admin/pages/scripts/pj-cadastrar.js
@@ -287,21 +287,20 @@ var PJCad = function() {
             });
 
             situacao();
-            $('#inativo').click(situacao);
-            $('#ativo').click(situacao);
+            $('#situacao').change(situacao);
             function situacao() {
-                alert('click');
-                if ($('#inativo').is(":checked")) {
+                if ($("input[name=situacao]:checked").val() === 'I') {
                     $('.reason').show();
                 } else {
                     $('.reason').hide();
                 }
             }
+            
 
             function validaData(value, element) {
                 var reg = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$/g;
                 return value.match(reg) ? true : false;
-            }
+            };
         }
     };
 }();
