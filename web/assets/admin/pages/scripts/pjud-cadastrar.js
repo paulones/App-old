a304e34d7d3112563932922ab6e55e7db68da037
paulones <paulones@paulones-X450LCP>
2014-08-19 01:11:19 -0300

    feito tela oca do processo judicial

--- web/assets/admin/pages/scripts/pjud-cadastrar.js
+++ web/assets/admin/pages/scripts/pjud-cadastrar.js
@@ -1,129 +1,141 @@
 var PjudCad = function() {
 
-    return {
-        init: function() {
+    var handleValidation = function() {
+        var form = $('#submit_form');
+        var error = $('.alert-danger', form);
+        var success = $('.alert-success', form);
 
-            if (!jQuery().bootstrapWizard) {
-                return;
+        form.validate({
+            doNotHideMessage: true, //this option enables to show the error/success messages on tab switch.
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block help-block-error', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            rules: {
+                nprocesso: {
+                    required: true,
+                },
+                nprocessoant: {
+                    required: false,
+                },
+                comarca: {
+                    required: true,
+                },
+                procurador: {
+                    required: false,
+                },
+                vara: {
+                    required: false,
+                },
+                varaant: {
+                    required: false,
+                },
+                especializacao: {
+                    required: false,
+                },
+                distribuicao: {
+                    required: false,
+                },
+                distribuicaodata: {
+                    required: false,
+                },
+                distribuicaofolha: {
+                    required: false,
+                },
+                decisaojuiz: {
+                    required: false,
+                },
+                decisaojuizdata: {
+                    required: false,
+                },
+                decisaojuizfolha: {
+                    required: false,
+                },
+                ncda: {
+                    required: true,
+                },
+                fatosgeradores: {
+                    required: false,
+                },
+                fundamentacao: {
+                    required: false,
+                },
+                datadeinscricao: {
+                    required: false,
+                },
+                discriminacaoimposto: {
+                    required: false,
+                },
+                discriminacaomulta: {
+                    required: false,
+                },
+                valorcausa: {
+                    required: false,
+                },
+                valoratualizado: {
+                    required: false,
+                },
+                notificacaoadministrativa: {
+                    required: false,
+                },
+                outrasinfos1: {
+                    required: false,
+                },
+                executado: {
+                    required: true,
+                },
+                cpf: {
+                    required: false,
+                },
+                cnpj: {
+                    required: false,
+                },
+                outrasinfos2: {
+                    required:false,
+                },
+                outrasinfos3: {
+                    required: false,
+                },
+            },
+            messages: {// custom messages for radio buttons and checkboxes
+
+            },
+            errorPlacement: function(error, element) { // render error placement for each input type
+                if (element.attr("name") == "executado") { // for uniform radio buttons, insert the after the given container
+                    error.insertAfter("#form_executado_error");
+                } else {
+                    var icon = $(element).parent('.input-icon').children('i');
+                    icon.removeClass('fa-check').addClass("fa-warning");
+                    icon.attr("data-original-title", error.text()).tooltip({'container': 'body'}); // for other inputs, just perform default behavior
+                }
+            },
+            invalidHandler: function(event, validator) { //display error alert on form submit   
+                success.hide();
+                error.show();
+                Metronic.scrollTo(error, -200);
+            },
+            highlight: function(element) { // hightlight error inputs
+                $(element)
+                        .closest('.form-group').removeClass('has-success').addClass('has-error'); // set error class to the control group
+            },
+            unhighlight: function(element) { // revert the change done by hightlight
+                $(element)
+                        .closest('.form-group').removeClass('has-error'); // set error class to the control group
+            },
+            success: function(label, element) {
+                var icon = $(element).parent('.input-icon').children('i');
+                $(element).closest('.form-group').removeClass('has-error'); // set success class to the control group
+                icon.removeClass("fa-warning");
+                icon.removeAttr("data-original-title");
+            },
+            submitHandler: function(form) {
+                success.show();
+                error.hide();
+                //add here some ajax code to submit your form or just call form.submit() if you want to submit the form without ajax
             }
 
-            var form = $('#submit_form');
-            var error = $('.alert-danger', form);
-            var success = $('.alert-success', form);
-
-            form.validate({
-                doNotHideMessage: true, //this option enables to show the error/success messages on tab switch.
-                errorElement: 'span', //default input error message container
-                errorClass: 'help-block help-block-error', // default input error message class
-                focusInvalid: false, // do not focus the last invalid input
-                rules: {
-                    username: {
-                        minlength: 5,
-                        required: true
-                    },
-                    password: {
-                        minlength: 5,
-                        required: true
-                    },
-                    rpassword: {
-                        minlength: 5,
-                        required: true,
-                        equalTo: "#submit_form_password"
-                    },
-                    //profile
-                    fullname: {
-                        required: true
-                    },
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    phone: {
-                        required: true
-                    },
-                    gender: {
-                        required: true
-                    },
-                    address: {
-                        required: true
-                    },
-                    city: {
-                        required: true
-                    },
-                    country: {
-                        required: true
-                    },
-                    //payment
-                    card_name: {
-                        required: true
-                    },
-                    card_number: {
-                        minlength: 16,
-                        maxlength: 16,
-                        required: true
-                    },
-                    card_cvc: {
-                        digits: true,
-                        required: true,
-                        minlength: 3,
-                        maxlength: 4
-                    },
-                    card_expiry_date: {
-                        required: true
-                    },
-                    'payment[]': {
-                        required: true,
-                        minlength: 1
-                    }
-                },
-                messages: {// custom messages for radio buttons and checkboxes
-                    'payment[]': {
-                        required: "Please select at least one option",
-                        minlength: jQuery.validator.format("Please select at least one option")
-                    }
-                },
-                errorPlacement: function(error, element) { // render error placement for each input type
-                    if (element.attr("name") == "gender") { // for uniform radio buttons, insert the after the given container
-                        error.insertAfter("#form_gender_error");
-                    } else if (element.attr("name") == "payment[]") { // for uniform radio buttons, insert the after the given container
-                        error.insertAfter("#form_payment_error");
-                    } else {
-                        error.insertAfter(element); // for other inputs, just perform default behavior
-                    }
-                },
-                invalidHandler: function(event, validator) { //display error alert on form submit   
-                    success.hide();
-                    error.show();
-                    Metronic.scrollTo(error, -200);
-                },
-                highlight: function(element) { // hightlight error inputs
-                    $(element)
-                            .closest('.form-group').removeClass('has-success').addClass('has-error'); // set error class to the control group
-                },
-                unhighlight: function(element) { // revert the change done by hightlight
-                    $(element)
-                            .closest('.form-group').removeClass('has-error'); // set error class to the control group
-                },
-                success: function(label) {
-                    if (label.attr("for") == "gender" || label.attr("for") == "payment[]") { // for checkboxes and radio buttons, no need to show OK icon
-                        label
-                                .closest('.form-group').removeClass('has-error').addClass('has-success');
-                        label.remove(); // remove error label here
-                    } else { // display success icon for other inputs
-                        label
-                                .addClass('valid') // mark the current input as valid and display OK icon
-                                .closest('.form-group').removeClass('has-error').addClass('has-success'); // set success class to the control group
-                    }
-                },
-                submitHandler: function(form) {
-                    success.show();
-                    error.hide();
-                    //add here some ajax code to submit your form or just call form.submit() if you want to submit the form without ajax
-                }
-
-            });
-
-            var handleTitle = function(tab, navigation, index) {
+        });
+        
+        var handleTitle = function(tab, navigation, index) {
                 var total = navigation.find('li').length;
                 var current = index + 1;
                 // set wizard title
@@ -144,7 +156,6 @@ var PjudCad = function() {
                 if (current >= total) {
                     $('#form_wizard').find('.button-next').hide();
                     $('#form_wizard').find('.button-submit').show();
-                    displayConfirm();
                 } else {
                     $('#form_wizard').find('.button-next').show();
                     $('#form_wizard').find('.button-submit').hide();
@@ -189,12 +200,33 @@ var PjudCad = function() {
                     });
                 }
             });
+    }
+
+    return {
+        init: function() {
+
+            if (!jQuery().bootstrapWizard) {
+                return;
+            }
+
+            handleValidation();
+            
+            executado();
+            $('#executado').change(executado);
+            $('#executado').click(executado);
+            function executado() {
+                if ($("input[name=executado]:checked").val() === 'cpf') {
+                    $('.cpf').show();
+                } else if ($("input[name=executado]:checked").val() === 'cnpj') {
+                    $('.cnpj').hide();
+                }
+            }
 
             $('#form_wizard').find('.button-previous').hide();
             $('#form_wizard .button-submit').click(function() {
                 alert('Finished! Hope you like it :)');
             }).hide();
-            
+
             $('.menu-pjud').addClass('active open');
             $('.menu-pjud a').append('<span class="selected"></span>');
             $('.menu-pjud a .arrow').addClass('open');
