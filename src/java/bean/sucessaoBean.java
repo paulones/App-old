b4c6e5a89d50216e85ba2c1e6c6db2d89992d78a
Pedro <Pedro@SGN-DESEN_10>
2014-08-20 17:19:53 -0300

    Estrutura da tela de sucessão!

--- src/java/bean/sucessaoBean.java
+++ src/java/bean/sucessaoBean.java
@@ -6,7 +6,10 @@
 
 package bean;
 
+import bo.PessoaJuridicaBO;
+import entidade.PessoaJuridica;
 import java.io.Serializable;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -17,7 +20,63 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 @ManagedBean(name = "sucessaoBean")
 public class sucessaoBean implements Serializable{
+    
+    private String succeed;
+    private PessoaJuridica sucedida;
+    private PessoaJuridica sucessora;
+    
+    private List<PessoaJuridica> pessoaJuridicaList;
+    
+    private PessoaJuridicaBO pessoaJuridicaBO;
+    
     public void init(){
+        succeed = "";
+        pessoaJuridicaBO = new PessoaJuridicaBO();
+        pessoaJuridicaList = pessoaJuridicaBO.findAll();
+    }
+    
+    public void cadastrarSucedida(){
         
     }
+    
+    public void cadastrarSucessora(){
+        
+    }
+    
+    public void suceder(){
+        
+    }
+
+    public String getSucceed() {
+        return succeed;
+    }
+
+    public void setSucceed(String succeed) {
+        this.succeed = succeed;
+    }
+
+    public PessoaJuridica getSucedida() {
+        return sucedida;
+    }
+
+    public void setSucedida(PessoaJuridica sucedida) {
+        this.sucedida = sucedida;
+    }
+
+    public PessoaJuridica getSucessora() {
+        return sucessora;
+    }
+
+    public void setSucessora(PessoaJuridica sucessora) {
+        this.sucessora = sucessora;
+    }
+
+    public List<PessoaJuridica> getPessoaJuridicaList() {
+        return pessoaJuridicaList;
+    }
+
+    public void setPessoaJuridicaList(List<PessoaJuridica> pessoaJuridicaList) {
+        this.pessoaJuridicaList = pessoaJuridicaList;
+    }
+    
 }
