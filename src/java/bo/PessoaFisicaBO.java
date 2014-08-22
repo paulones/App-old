ce0e708b88ec951824636cdf96dc9b9c1728b821
paulones <paulones@paulones-X450LCP>
2014-08-21 22:39:32 -0300

    feito alteração pra cpf diferente funcionar

--- src/java/bo/PessoaFisicaBO.java
+++ src/java/bo/PessoaFisicaBO.java
@@ -57,6 +57,15 @@ public class PessoaFisicaBO implements Serializable {
         return null;
     }
     
+    public PessoaFisica findByCPF (String cpf){
+        try {
+            return pessoaFisicaDAO.findByCPF(cpf);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
     public PessoaFisica findPessoaFisica(Integer id){
         try { 
             return pessoaFisicaDAO.findPessoaFisica(id);
