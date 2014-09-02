66651b5cd65a2960896910512cc83a36ddbefac9
paulones <paulones@paulones-X450LCP>
2014-09-02 16:15:13 -0300

    pondo os faceis graficos no js

--- web/assets/admin/pages/scripts/home.js
+++ web/assets/admin/pages/scripts/home.js
@@ -6,31 +6,6 @@ var Home = function() {
             return;
         }
 
-        var data = [];
-        var totalPoints = 250;
-
-        // random data generator for plot charts
-
-        function getRandomData() {
-            if (data.length > 0)
-                data = data.slice(1);
-            // do a random walk
-            while (data.length < totalPoints) {
-                var prev = data.length > 0 ? data[data.length - 1] : 50;
-                var y = prev + Math.random() * 10 - 5;
-                if (y < 0)
-                    y = 0;
-                if (y > 100)
-                    y = 100;
-                data.push(y);
-            }
-            // zip the generated y values with the x values
-            var res = [];
-            for (var i = 0; i < data.length; ++i)
-                res.push([i, data[i]])
-            return res;
-        }
-
         //Interactive Chart
 
         function interactiveChart() {
